3d052843c7d080a60ab190ec1350f8169b67e626
Nyasha Nyawo <nyasha@knowledgeobjects.com>
2026-08-11 09:54:46 +0200

    commit changes

--- src/main/java/com/codewithnyasha/store/SMSMessageService.java
+++ src/main/java/com/codewithnyasha/store/SMSMessageService.java
@@ -5,7 +5,8 @@ import org.springframework.stereotype.Service;
 @Service("sms")
 public class SMSMessageService implements NotificationService {
     @Override
-    public void processNotification() {
-        System.out.println("This is  the SMS Message Service");
+    public void processNotification()
+    {
+        System.out.println("This is  the SMS Message Service ===");
     }
 }
